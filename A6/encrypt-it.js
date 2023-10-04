8e1713a479b9b9f31ebdd17e5c6ea534179ccfad
yandereindica <bbdoney@gmail.com>
2023-10-03 18:49:22 -0700

    ch

--- A6/encrypt-it.js
+++ A6/encrypt-it.js
@@ -2,3 +2,44 @@
     console.log("Window loaded!");
 
 // Part II: add event listener to button click
+function handleClick()
+{
+    console.log("Button clicked!");
+}
+
+const encryptButton = document.getElementById("encrypt-it");
+encryptButton.addEventListener("click", handleClick);
+
+// Part III: implement shift cipher
+function shiftCipher(text) 
+{
+    text = text.toLowerCase();
+    let result = "";
+
+    for (let i = 0; i < text.length; i++)
+    {
+        if (text[i] < 'a' || text[i] > 'z')
+        {
+            result += text[i];
+        } else if (text[i] == 'z') {
+            result += 'a';
+        } else {
+            let letter = text.charCodeAt(i);
+            let resultLetter = String.fromCharCode(letter + 1);
+            result += resultLetter;
+        }
+    }
+    return result;
+}
+
+function handleClick()
+{
+    const inputText = document.getElementById("input-text").value;
+    const encryptedText = shiftCipher(inputText);
+    const resultElement = document.getElementById("result");
+    resultElement.textContent = encryptedText;
+}
+
+encryptButton.addEventListener("click", handleClick);
+
+// Note: the script tag linking this file to the HTML should be added in the HTML file.
