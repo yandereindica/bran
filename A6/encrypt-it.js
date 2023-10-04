a01e090613775f964d61bff6d58616ff52ac2b7c
yandereindica <bbdoney@gmail.com>
2023-10-03 18:31:18 -0700

    Update encrypt-it.js

--- A6/encrypt-it.js
+++ A6/encrypt-it.js
@@ -1,37 +1,43 @@
 // Part I: testing code that logs a message to the console
-console.log("Window loaded!");
+    console.log("Window loaded!");
 
 // Part II: add event listener to button click
-function handleClick() {
-console.log("Button clicked!");
+function handleClick()
+{
+    console.log("Button clicked!");
 }
 
 const encryptButton = document.getElementById("encrypt-button");
 encryptButton.addEventListener("click", handleClick);
 
 // Part III: implement shift cipher
-function shiftCipher(text) {
-text = text.toLowerCase();
-let result = "";
-for (let i = 0; i < text.length; i++) {
-if (text[i] < 'a' || text[i] > 'z') {
-result += text[i];
-} else if (text[i] == 'z') {
-result += 'a';
-} else {
-let letter = text.charCodeAt(i);
-let resultLetter = String.fromCharCode(letter + 1);
-result += resultLetter;
-}
-}
-return result;
+function shiftCipher(text) 
+{
+    text = text.toLowerCase();
+    let result = "";
+
+    for (let i = 0; i < text.length; i++)
+    {
+        if (text[i] < 'a' || text[i] > 'z')
+        {
+            result += text[i];
+        } else if (text[i] == 'z') {
+            result += 'a';
+        } else {
+            let letter = text.charCodeAt(i);
+            let resultLetter = String.fromCharCode(letter + 1);
+            result += resultLetter;
+        }
+    }
+    return result;
 }
 
-function handleClick() {
-const inputText = document.getElementById("input-text").value;
-const encryptedText = shiftCipher(inputText);
-const resultElement = document.getElementById("result");
-resultElement.textContent = encryptedText;
+function handleClick()
+{
+    const inputText = document.getElementById("input-text").value;
+    const encryptedText = shiftCipher(inputText);
+    const resultElement = document.getElementById("result");
+    resultElement.textContent = encryptedText;
 }
 
 encryptButton.addEventListener("click", handleClick);
