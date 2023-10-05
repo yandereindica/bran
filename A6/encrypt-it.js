a1f9e7d902f3593b3c6cce40fbbfaa6a1405c5e0
yandereindica <bbdoney@gmail.com>
2023-10-04 23:16:33 -0700

    Update encrypt-it.js

--- A6/encrypt-it.js
+++ A6/encrypt-it.js
@@ -16,17 +16,46 @@
     */
     function init() {
         const encryptButton = document.getElementById("encrypt-it");
+        const resetButton = document.getElementById("reset");
         encryptButton.addEventListener("click", handleEncrypt);
+        resetButton.addEventListener("click", handleReset);
+
     // Note: In this function, we usually want to set up our event handlers
     // for UI elements on the page.
     }
 
-    function handleEncrypt() {
-        console.log("Button clicked!");
-    }
-    
     // Add any other functions in this area (you should not implement your
     // entire program in the init function, for similar reasons that
     // you shouldn't write an entire Java program in the main method).
-    
-    })();
+
+    function handleReset() { 
+        const inputTextArea = document.getElementById("input-text"); 
+        inputTextArea.value = "";
+    }
+
+    function handleEncrypt() { 
+        const inputTextArea = document.getElementById("input-text");
+        const resultParagraph = document.getElementById("result");
+        const inputText = inputTextArea.value;
+        const encryptedText = shiftCipher(inputText);
+        resultParagraph.textContent = "Encrypted Message: " + encryptedText;
+    } 
+
+    function shiftCipher(text) { 
+        text = text.toLowerCase();
+        let result = "";
+
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] < 'a' || text[i] > 'z') {
+                result += text[i];
+            } else if (text[i] === 'z') {
+                result += 'a'; 
+            } else { 
+                let letter = text.charCodeAt(i);
+                let resultLetter = String.fromCharCode(letter + 1);
+                result += resultLetter;
+            }
+        } 
+        return result;
+     }
+})();
